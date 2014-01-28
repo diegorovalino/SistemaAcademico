9f2367510688cc9bd37d2fa4c34e69401bee028a
JennyRea <jerp_1603@hotmail.com>
2014-01-28 11:37:08 -0500

    Creamos modulo7 Matriculas

---
+++ wsgi/openshift/modulo7/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from openshift.modulo7.models import *
+
+# Register your models here.
+
+admin.site.register(Carreras)
+admin.site.register(Estados)
+admin.site.register(Periodos)
+admin.site.register(Estudiantes)
+
+
