32ba5b1f82745c8fc81d836cf62f052479e761e4
Aplicaciones1 <diego_xs1618@hotmail.com>
2014-01-29 01:19:59 -0500

    Update settings.py

--- wsgi/openshift/settings.py
+++ wsgi/openshift/settings.py
@@ -71,7 +71,8 @@ USE_L10N = True
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
 
-MEDIA_ROOT = os.environ.get('MEDIA_ROOT',os.path.join(os.path.dirname(__file__), 'media'))
+#MEDIA_ROOT = os.environ.get('MEDIA_ROOT',os.path.join(os.path.dirname(__file__), 'media'))
+MEDIA_ROOT = os.environ.get('OPENSHIFT_DATA_DIR', '')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
